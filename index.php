481565077c539bb12bdf418b642317765b0492b6
Andrey Evsyukov <andrey.evsyukov@gmail.com>
2014-07-23 19:34:20 +0400

    minor fixes

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
 date_default_timezone_set('Europe/Moscow');
 
-require_once './ini.php';
-require_once './class/http.php';
-require_once './class/db.php';
+require_once dirname(__FILE__).'/ini.php';
+require_once dirname(__FILE__).'/class/http.php';
+require_once dirname(__FILE__).'/class/db.php';
 
 
 
-require_once './parsers/appadvice_updated_free.php';
-require_once './parsers/appadvice_updated_paid.php';
-require_once './parsers/appadvice_new_social_free.php';
+require_once dirname(__FILE__).'/parsers/appadvice_updated_free.php';
+require_once dirname(__FILE__).'/parsers/appadvice_updated_paid.php';
+require_once dirname(__FILE__).'/parsers/appadvice_new_social_free.php';
 
-require_once './parsers/google.php';
-require_once './parsers/collection_topselling_new_free.php';
-require_once './parsers/collection_topselling_new_paid.php';
-require_once './parsers/game_collection_topselling_new_free.php';
-require_once './parsers/game_collection_topselling_new_paid.php';
-require_once './parsers/social_collection_topselling_free.php';
-require_once './parsers/social_collection_topselling_paid.php';
+require_once dirname(__FILE__).'/parsers/google.php';
+require_once dirname(__FILE__).'/parsers/collection_topselling_new_free.php';
+require_once dirname(__FILE__).'/parsers/collection_topselling_new_paid.php';
+require_once dirname(__FILE__).'/parsers/game_collection_topselling_new_free.php';
+require_once dirname(__FILE__).'/parsers/game_collection_topselling_new_paid.php';
+require_once dirname(__FILE__).'/parsers/social_collection_topselling_free.php';
+require_once dirname(__FILE__).'/parsers/social_collection_topselling_paid.php';
 
 
 
