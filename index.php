264b06daf0d08eaebfd1ee113e828f78104c70ee
Alexey Spiridonov <forspall@gmail.com>
2014-07-27 16:02:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ date_default_timezone_set('Europe/Moscow');
 
 include('./PRFLR.SDK.PHP/prflr.php');
 
+PRFLR::init('hive', 'tOa0U4uBphHNQZUO7yWajR1SVoUmUWR1');
 
 require_once dirname(__FILE__).'/ini.php';
 require_once dirname(__FILE__).'/class/http.php';
