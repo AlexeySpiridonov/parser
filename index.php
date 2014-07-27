ee0070852fdb3fc30f841bd08d6b310f867c1a6f
Alexey Spiridonov <forspall@gmail.com>
2014-07-27 17:34:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,13 +11,13 @@ require_once dirname(__FILE__).'/class/db.php';
 require_once __DIR__ .'/class/Helper.php';
 
 //others
-require_once __DIR__ . '/parsers/spark.php';
-require_once __DIR__ . '/parsers/f6s.php';
+require_once __DIR__ . '/parsers/macradar_ru.php';
 require_once __DIR__ . '/parsers/f6s_job.php';
 require_once __DIR__ . '/parsers/hh_vacancy_novosib.php';
 require_once __DIR__ . '/parsers/rusbase.php';
 require_once __DIR__ . '/parsers/rusbase_invest.php';
-require_once __DIR__ . '/parsers/macradar_ru.php';
+require_once __DIR__ . '/parsers/spark.php';
+require_once __DIR__ . '/parsers/f6s.php';
 
 //apple
 require_once dirname(__FILE__).'/parsers/appadvice_updated_free.php';
