07f75bc04b4058f72481d9d30fb38495ada0dae2
Andrey <tp123@bk.ru>
2014-07-27 11:14:06 +0400

    Create ini.php

---
+++ ini.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Created by PhpStorm.
+* User: ssl
+* Date: 21.07.14
+* Time: 15:11
+*/
+
+define ("DB_LOGIN" , "root");
+define ("DB_PASSWORD" , "ntkrbntkrb69");
+define ("DB_HOST" , "127.0.0.1");
+define ("DB_NAME" , "app_market");
