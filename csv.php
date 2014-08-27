8dab4129d0ce3e1557f04f924512b460945fa044
Alexey Spiridonov <forspall@gmail.com>
2014-08-27 18:27:37 +0400

    Update csv.php

--- csv.php
+++ csv.php
@@ -16,4 +16,62 @@ $all = $db->getReport();
 $fp = fopen( dirname(__FILE__) . '/reports/list-dif-'. date("Y-m-d") . '.csv', 'w');
 foreach($all as $item){
     fputcsv($fp, array($item['email'], $item['name']) );
+    $new_emails[]=$item['email'];
+    $new_names[]=$item['name'];
+}
+
+
+
+
+// Ваш ключ доступа к API (из Личного Кабинета)
+$api_key = "5aqr6s55iwbfk8j5uxqy5zj3nehckto7ubr7e18y";
+
+
+// Список, куда их добавить
+$list = "4068999";
+
+// Создаём POST-запрос
+$POST = array (
+  'api_key' => $api_key,
+  'field_names[0]' => 'email',
+  'field_names[1]' => 'Name',
+  'field_names[21]' => 'email_list_ids'
+);
+for ($i=0;$i<5;$i++){
+  $POST['data[' . $i .'][0]'] = $new_emails[$i];
+  $POST['data[' . $i .'][1]'] = iconv('cp1251', 'utf-8', $new_names[$i]);
+  $POST['data[' . $i .'][2]'] = $list;
+}
+
+// Устанавливаем соединение
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $POST);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+curl_setopt($ch, CURLOPT_URL, 
+            'http://api.unisender.com/ru/api/importContacts?format=json');
+$result = curl_exec($ch);
+
+if ($result) {
+  // Раскодируем ответ API-сервера
+  $jsonObj = json_decode($result);
+
+  if(null===$jsonObj) {
+    // Ошибка в полученном ответе
+    echo "Invalid JSON";
+
+  }
+  elseif(!empty($jsonObj->error)) {
+    // Ошибка импорта
+    echo "An error occured: " . $jsonObj->error . "(code: " . $jsonObj->code . ")";
+
+  } else {
+    // Новые подписчики успешно добавлены
+    echo "Success! Added " . $jsonObj->result->new_emails . " new e-mail addresses";
+
+  }
+} else {
+  // Ошибка соединения с API-сервером
+  echo "API access error";
 }
