4933a5722261a9955839fe7c86218679cc1badc5
Alexey Spiridonov <forspall@gmail.com>
2015-04-15 13:39:56 +0500

    Update test.php

--- mailers/test.php
+++ mailers/test.php
@@ -1,7 +1,31 @@
 <?php
+return ""
+<p>Hi, {$name},
+<p>My name is Alex, I am from 2Hive, a company which provides SaaS for Content Moderation.  
+ <p>
+<p>2Hive was created to solve issues with moderation for other companies in a simple, easy and accurate way. We provide the perfect balance between efficiency and low cost while monitoring and helping you remove any undesired content from your project.
+<p> 
+<p>What do we offer?
+<p>There are 2 options for you to choose from:
+<br>A.  2Hive, as a SaaS solution, provides you with Web Tools such as API and SDK to assist in managing and controlling your own existing Moderation Team in a more efficient manner. 
+<br>B. 2Hive provides you a Moderation Team that monitors and corrects any problems with Content Moderation.
+<p> 
+<p>Using our services to deal with your moderation needs will free you up and allow you to focus on what really matters to you and your business.
+<p>
+<p>For more information about 2Hive, please check out our website at the following link for pricing and additional information: http://2hive.org
+<p>Test our services with a free trial by signing up below and moderate up to 1,000 posts on your website or application: http://2hive.org/project/signup
+<p>
+<p>Please do not hesitate to contact me personally for more information. I will be happy to discuss and answer any question you may have.
+<p>
+<p>I look forward to hearing from you and hope to have the opportunity to work with you in the near future.
 
+<p>-- 
+<br>Kind regards,
+<br>Alex 
+<br>info@2hive.org";
+/*
 return
     "This is \$email var: {$email}<br/>".
     "This is \$name var: {$name}<br/>";
-
+*/
 ?>
