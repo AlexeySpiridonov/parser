7a8433011b187e1a790cf235b48302056cb5152e
Alexey Spiridonov <forspall@gmail.com>
2015-04-17 09:34:28 +0500

    Update Mailer.php

--- mailers/Mailer.php
+++ mailers/Mailer.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(dirname(__FILE__) . '/MailHelper.php');
-include(dirname(__FILE__) . '../PRFLR.SDK.PHP/prflr.php');
+include(dirname(__FILE__) . '/../PRFLR.SDK.PHP/prflr.php');
 
 class Mailer
 {
