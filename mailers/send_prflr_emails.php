fc5b518cf029d6903dcc09192476fdf10c3bc66f
Alexey Spiridonov <forspall@gmail.com>
2015-07-27 16:37:08 +0500

    Create send_prflr_emails.php

---
+++ mailers/send_prflr_emails.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+mb_internal_encoding("UTF-8");
+set_time_limit(0);
+require_once(dirname(__FILE__) . '/Mailer.php');
+$date = date("Y-m-d", strtotime('-1 day'));
+$filename = "/var/www/parser/reports/list-dif-{$date}.csv";
+$template = "/var/www/parser/mailers/prflr_letter.php";
+$mailer = new Mailer($filename, $template, "Analyze application performance by using PRFLR");
+$mailer->send();
