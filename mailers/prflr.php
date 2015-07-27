b82c82a1d1dc3a017e97c7271d51ff34a14c193f
Alexey Spiridonov <forspall@gmail.com>
2015-07-27 16:39:02 +0500

    Create prflr.php

---
+++ mailers/prflr.php
@@ -0,0 +1,17 @@
+<?php
+return "
+<br>Hello *username*!
+<br>
+<br>If you are a programmer this can be interesting for you - A convenient SaaS helping you to analyze performance of web, mobile and desktop applications.
+<br>
+<br>It measures performance on the fly, right in production! No overhead, as we use UDP.
+<br>
+<br>Spend 5 minutes to setup it and you are ready to use it!
+<br>
+<br>Please refer to this link http://prflr.org
+<br>
+<br>Sorry for the spam.
+<br>
+<br>PRFLR team
+";
+?>
