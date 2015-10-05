0f7c8f3cf73063bbf1f9fc3454d0be028477ad0f
Alexey Spiridonov <forspall@gmail.com>
2015-10-05 18:30:37 +0500

    Update first_letter.php

--- mailers/first_letter.php
+++ mailers/first_letter.php
@@ -1,27 +1,22 @@
 <?php
 return "
-<p>Hi,
-<p>My name is Alex, I am  CEO at 2Hive (http://2hive.org), our company provides Content Moderation solution as a SaaS.
- <p>
-<p>2Hive was created to solve issues with moderation for other companies in a simple, easy and accurate way. We provide the perfect balance between efficiency and low cost while monitoring and helping you remove any undesired content from your project.
-<p> 
-<p>What do we offer?
-<p>There are 2 options for you to choose from:
-<br>--- 2Hive, as a SaaS solution, provides you with Web Tools such as API and SDK to assist in managing and controlling your own existing Moderation Team in a more efficient manner. 
-<br>--- 2Hive provides you a Moderation Team that monitors and corrects any problems with Content Moderation.
-<p> 
-<p>Using our services to deal with your moderation needs will free you up and allow you to focus on what really matters to you and your business.
-<p>
-<p>For more information about 2Hive, please check out our website at the following link for pricing and additional information: http://2hive.org
-<p>Test our services with a free trial by signing up below and moderate up to 1,000 posts on your website or application: http://2hive.org/project/signup
-<p>
-<p>Please do not hesitate to contact me personally for more information. I will be happy to discuss and answer any question you may have.
-<p>
-<p>I look forward to hearing from you and hope to have the opportunity to work with you in the near future.
+<p>Greetings,
 
-<p>-- 
-<br>Kind regards,
-<br>Alex 
+<p>My name is Alex, I am  CEO at 2Hive LLC, our company provides Content Moderation solution as a SaaS.
+
+<p>We offer a solution which helps managing/moderating any sorts of User Generated Content in a simple and efficient way. This solution can be used in two ways – web tools only, and a complete set of personnel and instruments for moderating user generated content in your project.
+
+<p> - The first approach helps to dramatically increase the efficiency of your own moderation team with some pr-automation and content sorting techniques implemented, our solution will only needs to be connected to your platform using available SDK and API. 
+
+<p> - The other approach we offer is to completely outsource the content moderation needs to our company. The rates and conditions are highly competitive, and it is really easy to track the efficiency of our work. 
+
+<p>In any case - there are no any set up fees, nor any hidden charges. In addition, there is a free paln  available to test our solution. 
+
+<p>For any further information about 2Hive solution and team please contact us directly, or visit our web site for the more details. 
+
+<p>Looking forward to hear from you,
+
+<br>Kind regards, Alex 
 <br>info@2hive.org
 <br>http://2hive.org
 ";
